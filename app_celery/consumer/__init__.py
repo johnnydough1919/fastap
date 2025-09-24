69ed41e4be5864dcabd7087e6aea0b528c06b0fe
free <233485754+amrunje@users.noreply.github.com>
2025-09-24 10:02:02 +0800

    opt@强化celery任务模块发现

--- app_celery/consumer/__init__.py
+++ app_celery/consumer/__init__.py
@@ -1,18 +1,29 @@
 """
 消费者
 """
+import re
 from pathlib import Path
 
 from app_celery import make_celery
 
 
 def autodiscover_task_modules(
-        tasks_name: str = "tasks",
-        tasks_module: str = "app_celery.consumer.tasks",
+        task_name: str = "tasks",
+        task_module: str = "app_celery.consumer.tasks",
 ) -> list:
-    here = Path(__file__).parent
-    tasks_dir = here.joinpath(tasks_name)
-    return [f"{tasks_module}.{p.stem}" for p in tasks_dir.rglob("*.py") if p.stem != "__init__"]
+    """
+    自动发现任务模块
+    - 可在模块中加入`_active = False`来取消激活
+    """
+    task_modules = []
+    active_pat = re.compile(r"^_active\s*=\s*False\s*(?:#.*)?$", re.MULTILINE)
+    for p in Path(__file__).parent.joinpath(task_name).rglob("*.py"):
+        if p.stem == "__init__":
+            continue
+        if active_pat.search(p.read_text(encoding="utf-8")):
+            continue
+        task_modules.append(f"{task_module}.{p.stem}")
+    return task_modules
 
 
 celery_app = make_celery(
